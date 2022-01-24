6bfc173d127d2ba3cf4e762112c46a74995892cd
ByMikiii <mikixr1@gmail.com>
2022-01-24 10:59:44 +0100

    JS localstorage test2

--- myScript.js
+++ myScript.js
@@ -1,9 +1,11 @@
    var number = 0;
    var pnumber = 0;
+
     
     function myFunction() {
-      localStorage.setItem(number, pnumber);
+      localStorage.setItem('number', pnumber);
       number+= 1; 
-      document.getElementById("text").innerHTML = number;
       pnumber = localStorage.getItem(number);
+      document.getElementById("cislo").innerHTML = number;
     }
+
