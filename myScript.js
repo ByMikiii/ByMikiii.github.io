a6fc1a86bf1f24f11415f3bda8682c0882eac1a6
ByMikiii <mikixr1@gmail.com>
2022-01-24 13:27:27 +0100

    JS Clicks Working

--- myScript.js
+++ myScript.js
@@ -1,11 +1,22 @@
    var number = 0;
-   var pnumber = 0;
 
-    
+   /*
     function myFunction() {
-      localStorage.setItem('number', pnumber);
-      number+= 1; 
-      pnumber = localStorage.getItem(number);
+      number+= 1;
       document.getElementById("cislo").innerHTML = number;
+      
+      localStorage.setItem("cislo", storedValue);
+      var storedValue = localStorage.getItem("cislo");
+      console.log(storedValue);
     }
-
+    */
+    document.getElementById("clicks").innerHTML = localStorage.clicks;
+    
+    function clickCounter(){
+      if (localStorage.hasOwnProperty('clicks')) {
+        localStorage.clicks = Number(localStorage.clicks)+1;
+    } else {
+        localStorage.clicks = 0;
+    }
+    document.getElementById("clicks").innerHTML = localStorage.clicks;
+    }
