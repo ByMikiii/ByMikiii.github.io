ac075b53926c1a4aa0162486e4db209b90690ba4
ByMikiii <mikixr1@gmail.com>
2022-01-31 14:27:12 +0100

    ...

--- myScript.js
+++ myScript.js
@@ -1,22 +1,12 @@
-   var number = 0;
+var number = 0;
 
-   /*
-    function myFunction() {
-      number+= 1;
-      document.getElementById("cislo").innerHTML = number;
-      
-      localStorage.setItem("cislo", storedValue);
-      var storedValue = localStorage.getItem("cislo");
-      console.log(storedValue);
-    }
-    */
-    document.getElementById("clicks").innerHTML = localStorage.clicks;
-    
-    function clickCounter(){
-      if (localStorage.hasOwnProperty('clicks')) {
-        localStorage.clicks = Number(localStorage.clicks)+1;
-    } else {
-        localStorage.clicks = 0;
-    }
-    document.getElementById("clicks").innerHTML = localStorage.clicks;
-    }
+document.getElementById("clicks").innerHTML = localStorage.clicks;
+
+function clickCounter() {
+  if (localStorage.hasOwnProperty("clicks")) {
+    localStorage.clicks = Number(localStorage.clicks) + 1;
+  } else {
+    localStorage.clicks = 0;
+  }
+  document.getElementById("clicks").innerHTML = localStorage.clicks;
+}
